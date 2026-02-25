115753d1fcc3983988ccc72344f4d347abb66fd8
pkeinde6 <pkeinde6@gmail.com>
2026-02-25 01:56:46 +0000

    ux: align validate buttons to the right for right-handed users

--- src/components/WrittenQuestion.vue
+++ src/components/WrittenQuestion.vue
@@ -30,6 +30,7 @@
       />
     </div>
 
+    <div class="flex justify-end">
     <button
       @click="validate"
       :disabled="!typed.trim()"
@@ -38,6 +39,7 @@
       <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5" /></svg>
       Valider
     </button>
+    </div>
   </div>
 </template>
 
--- src/components/question.vue
+++ src/components/question.vue
@@ -19,6 +19,7 @@
       </li>
     </ul>
 
+    <div class="flex justify-end">
     <button
       @click="validate"
       :disabled="!selected"
@@ -27,6 +28,7 @@
       <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5" /></svg>
       Valider
     </button>
+    </div>
   </div>
 </template>
 
